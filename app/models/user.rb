d577c7073ad3ac0845b0b84175eafeb9e8da3162
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-10-30 21:18:19 -0400

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+end
+
+# Array values in the parameter to `Gem.paths=` are deprecated.
+# Please use a String or nil.
+# An Array ({"GEM_PATH"=>["/Users/nashat/.rvm/gems/ruby-2.6.1", "/Users/nashat/.rvm/rubies/ruby-2.6.1/lib/ruby/gems/2.6.0"]}) was passed in from bin/rails:3:in `load'
+# Running via Spring preloader in process 7668
